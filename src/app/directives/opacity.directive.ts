4b916cfe8f4dfa279264936d14f5e3c48f06ddcf
André Lopes <andrels7@icloud.com>
2024-11-25 17:36:41 -0300

    Update opacity.directive.ts

--- src/app/directives/opacity.directive.ts
+++ src/app/directives/opacity.directive.ts
@@ -6,11 +6,13 @@ import { Directive, HostBinding, HostListener } from '@angular/core';
 export class OpacityDirective {
 
   @HostBinding('style.opacity') Opacidade: string = '';
+  @HostBinding('style.transition') Transicao: string = '';
 
   constructor() { }
 
   @HostListener('mouseover') aoPassarMouse() {
     this.Opacidade = '0.8';
+    this.Transicao = '0.5s';
   }
 
   @HostListener('mouseout') aoTirarMouse() {
