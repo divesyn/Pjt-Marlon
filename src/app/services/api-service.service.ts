e4d9413daed5acf0b1d2712c51324234a6b99ee5
André Lopes <andrels7@icloud.com>
2024-11-26 09:09:55 -0300

    Update api-service.service.ts

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -11,8 +11,4 @@ export class ApiServiceService {
   getListaPaises() {
     return this.http.get('https://restcountries.com/v3.1/all')
   }
-
-  getPaisporId(id: any){
-    return this.http.get('https://restcountries.com/v3.1/alpha/' + id)
-  }
 }
