ad319b294ea511fbe4ca2e421d27e8b74fbf1ac6
André Lopes <andrels7@icloud.com>
2024-11-26 09:07:55 -0300

    Create paispor-id.service.ts

---
+++ src/app/services/paispor-id.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisporIdService {
+
+  constructor(private http: HttpClient) { }
+
+  getPaisporId(id: any){
+    return this.http.get('https://restcountries.com/v3.1/alpha/' + id)
+  }
+}
